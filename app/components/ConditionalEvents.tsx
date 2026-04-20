a6d30e029ffe16e9b5875e7146005398d6c706c0
skeilson <shane.keilson@grafana.com>
2026-04-20 06:57:01 -0600

    fix: missing brackets in ConditionalEvents component

--- app/components/ConditionalEvents.tsx
+++ app/components/ConditionalEvents.tsx
@@ -107,7 +107,7 @@ export default function ConditionalEvents({
                           </button>
                         ))}
                       </div>
-                    ))}
+                    )}
 
                     {field.type === 'text' && (
                       <input
